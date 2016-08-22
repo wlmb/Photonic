775fcb2a86216c66f08e98b3610198c1e3819b65
Luis Mochan <mochan@fis.unam.mx>
2016-08-22 09:16:26 -0500

    Add Wave.pm to calculate wave operator.
    Assume symmetrical tensor. No chirality yet.

---
+++ lib/Photonic/Retarded/Wave.pm
@@ -0,0 +1,100 @@
+=head1 NAME
+
+Photonic::Retarded::Wave
+
+=head1 VERSION
+
+version 0.006
+
+=head1 SYNOPSIS
+
+   use Photonic::Retarded::Wave;
+   my $W=Photonic::Retarded::Wave->new(metric=>$m, nh=>$nh);
+   my $WaveTensor=$W->evaluate($epsB);
+
+=head1 DESCRIPTION
+
+Calculates the macroscopic wave operator for a given fixed
+Photonic::Retarded::Metric structure as a function of the dielectric
+functions of the components.
+
+=head1 METHODS
+
+=over 4
+
+=item * new(metric=>$m, nh=>$nh, small=>$small, keepStates=>$k)
+
+Initializes the structure.
+
+$m Photonic::Retarded::Metric describing the structure and some parametres.
+
+$nh is the maximum number of Haydock coefficients to use.
+
+$small is the criteria of convergence (default 1e-7)
+
+$k is a flag to keep states in Haydock calculations (default 0)
+
+=item * evaluate($epsB)
+
+Returns the macroscopic wave operator for a given value of the
+dielectric functions of the particle $epsB. The host's
+response $epsA is taken from the metric.  
+
+=back
+
+=head1 ACCESORS (read only)
+
+=over 4
+
+=item * waveOperator
+
+The macroscopic wave operator of the last operation
+
+=item * All accesors of Photonic::Retarded::Green
+
+
+=back
+
+=cut
+
+package Photonic::Retarded::Wave;
+$Photonic::Retarded::Green::VERSION = '0.006';
+use namespace::autoclean;
+use PDL::Lite;
+use PDL::NiceSlice;
+use PDL::Complex;
+use PDL::MatrixOps;
+use Storable qw(dclone);
+use PDL::IO::Storable;
+#use Photonic::Retarded::AllH;
+use Moose;
+use Photonic::Types;
+
+extends 'Photonic::Retarded::Green'; 
+
+has 'waveOperator' =>  (is=>'ro', isa=>'PDL::Complex', init_arg=>undef,
+             writer=>'_waveOperator',   
+             documentation=>'Wave operator from last evaluation');
+
+around 'evaluate' => sub {
+    my $orig=shift;
+    my $self=shift;
+    my $green=$self->$orig(@_);
+    #make a real matrix from [[R -I][I R]] to solve complex eq.
+    my $greenreim=$green->re->append(-$green->im)
+       ->glue(1,$green->im->append($green->re))->sever;
+    my($lu, $perm, $par)=$greenreim->lu_decomp;
+    my $d=$self->geometry->ndims;
+    my $idreim=identity($d)->glue(1,PDL->zeroes($d,$d))->mv(0,-1);
+    my $wavereim=lu_backsub($lu,$perm,$par,$idreim);
+    print "$wavereim\n";
+    my $wave=$wavereim->reshape($d,2,$d)->mv(1,0)->complex;
+    _waveOperator($wave);
+    return $wave;
+};
+
+__PACKAGE__->meta->make_immutable;
+    
+1;
+
+__END__
