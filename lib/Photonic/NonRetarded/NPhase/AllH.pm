4759d98a585a73a8e2c698a5eeee08ba44052eba
Luis Mochan <mochan@fis.unam.mx>
2018-12-01 23:06:40 -0600

    Use euclidean EProd in NP::AllH and reorthogonalize

--- lib/Photonic/NonRetarded/NPhase/AllH.pm
+++ lib/Photonic/NonRetarded/NPhase/AllH.pm
@@ -87,7 +87,7 @@ use namespace::autoclean;
 use Machine::Epsilon;
 use PDL::Lite;
 use PDL::NiceSlice;
-use Photonic::Utils qw(HProd);
+use Photonic::Utils qw(EProd);
 use Moose;
 
 extends 'Photonic::NonRetarded::NPhase::OneH';
@@ -126,8 +126,6 @@ has 'Accuracy'=>(is=>'ro', default=>sub{machine_epsilon()},
 
 sub BUILD {
     my $self=shift;
-    die "Don't know yet how to reorthogonalize" if $self->reorthogonalize;
-    # Can't reorthogonalize without previous states
     $self->_keepstates(1) if  $self->reorthogonalize;
 }
 
@@ -182,31 +180,34 @@ sub _save_a {
 
 sub _checkorthogonalize 
 {
-    return; #don't know how to reorthogonalize with complex haydock's
     my $self=shift;
     my $nextState=$self->nextState;
     return unless defined $nextState;
     my $a=PDL->cat($self->as);
-    die "Didn't build a complex pdl"
+    die "Didn't build a complex pdl of a's"
          unless ref($a) eq 'PDL::Complex' && $a->dim(0) == 2;
     my $b=PDL->cat($self->bs);
+    die "Didn't build a complex pdl of b's"
+         unless ref($b) eq 'PDL::Complex' && $b->dim(0) == 2;
+    my $a_abs=$a->Cabs;
+    my $b_abs=$b->Cabs;
     my $n=$self->iteration;
     $self->_previous_W(my $previous_W=$self->current_W);
     $self->_current_W(my $current_W=$self->next_W);
     my $next_W=PDL->pdl([]);
     if($n>=2){
 	$next_W=(
-	    $b->(1:-1)
+	    $b_abs->(1:-1)
 	    *
 	    $current_W->(1:-1) 
-	    + ($a->(0:-2)
-	       -$a->(($n-1)))
+	    + ($a_abs->(0:-2)
+	       -$a_abs->(($n-1)))
 	    *$current_W->(0:-2)  
-	    -$b->(($n-1))
+	    -$b_abs->(($n-1))
 	    *$previous_W);
-	$next_W->(1:-1)+=$b->(1:-2)*$current_W->(0:-3) if ($n>=3);
+	$next_W->(1:-1)+=$b_abs->(1:-2)*$current_W->(0:-3) if ($n>=3);
 	$next_W+=_sign($next_W)*2*$self->Accuracy;
-	$next_W/=$self->next_b;
+	$next_W/=$self->next_b->Cabs;
     }
     $next_W=$next_W->append($self->Accuracy) if $n>=1;
     $next_W=$next_W->append(1);
@@ -219,22 +220,25 @@ sub _checkorthogonalize
     my $currentState=$self->currentState;
     pop(@{$states}); #should be currentState;
     my ($oldnext, $oldcurrent)=($nextState, $currentState);
+    my $sign=1; #Magic sign, notes MQ
+    #I change the sign of all EProd's save the first CHECK!
     for my $s (@{$states}){
-	($nextState,$currentState)=map {$_-$s*HProd($s, $_)}
+	($nextState,$currentState)=map {$_-$s*$sign*EProd($s, $_)}
 	($nextState, $currentState);
+	$sign=-1;
     }
     #warn "Current change: ", ($currentState-$oldcurrent)->Cabs2->sum, "\n";
     #warn "Next change: " . ($nextState-$oldnext)->Cabs2->sum, "\n";
-    my $sc=sqrt($currentState->Cabs2->sum);
+    my $sc=-EProd($currentState, $currentState)->sqrt; #normalization
     # Is there a danger that $sc<$self->smallH?
-#    print "scold $scold sc $sc\n";
-    $currentState/=$sc;
+    # print "scold $scold sc $sc\n";
+    $currentState/=$sc; #normalize
     $self->_currentState($currentState);
     push @{$self->states}, $currentState;
     #necessary?:
-    $nextState-=$currentState*HProd($currentState, $nextState);
-    my $sn=sqrt($nextState->Cabs2->sum);
-    $nextState/=$sn;
+    $nextState-=-$currentState*EProd($currentState, $nextState); #orthogonalize
+    my $sn=-EProd($nextState, $nextState)->sqrt; #norm
+    $nextState/=$sn; #normalize
     $self->_nextState($nextState);
     $current_W(0:-2).=$self->Accuracy;
     $next_W(0:-2).=$self->Accuracy;
