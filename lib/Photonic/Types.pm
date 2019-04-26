b184e51ca10ed2c0bef55ab2a5fa4aedba538377
Luis Mochan <mochan@fis.unam.mx>
2019-04-26 12:19:35 -0500

    Add new AllHSave types to Photonic::Types

--- lib/Photonic/Types.pm
+++ lib/Photonic/Types.pm
@@ -66,6 +66,16 @@ subtype 'Photonic::Types::AllHSave' =>
   where { $_->keepStates == 1 },
     message { "Can't calculate fields if you don't keepStates" };
 
+subtype 'Photonic::Types::LE::NR2::AllHSave' =>
+  as 'Photonic::LE::NR2::AllH',
+  where { $_->keepStates == 1 },
+    message { "Can't calculate fields if you don't keepStates" };
+
+subtype 'Photonic::Types::LE::S::AllHSave' =>
+  as 'Photonic::LE::S::AllH',
+  where { $_->keepStates == 1 },
+    message { "Can't calculate fields if you don't keepStates" };
+
 subtype 'Photonic::Types::WE::R2::AllHSave' =>
   as 'Photonic::WE::R2::AllH',
   where { $_->keepStates == 1 },
