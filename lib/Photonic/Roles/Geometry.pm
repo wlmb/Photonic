4bae13568dcb2925f5f2d6d639ab5e35578feee8
Luis Mochan <mochan@fis.unam.mx>
2021-05-09 20:49:04 -0500

    Correct replace i*... by i()*... in Roles::Geometry

--- lib/Photonic/Roles/Geometry.pm
+++ lib/Photonic/Roles/Geometry.pm
@@ -232,8 +232,8 @@ sub _build_cUnitPairs {
     my @cpairs;
     for my $i(0..$nd-1){ #build pairs of vectors
 	for my $j($i+1..$nd-1){
-	    my $vc=($units->[$i]+i*$units->[$j]);
-	    my $vcn=sqrt(($units->[$i]+i*$units->[$j])->Cabs2->sumover);
+	    my $vc=($units->[$i]+i()*$units->[$j]);
+	    my $vcn=sqrt(($units->[$i]+i()*$units->[$j])->Cabs2->sumover);
 	    my $vp=$vc*(1/$vcn);
 	    push @cpairs, $vp;
 	}
