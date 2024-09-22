d809fa00509ac4053a339501c1f973e2d4d0065f
Luis Mochan <mochan@fis.unam.mx>
2024-09-22 17:30:39 -0600

    Fix versions and add Test::Version to pre-requisites

--- lib/Photonic/LE/ST/Haydock.pm
+++ lib/Photonic/LE/ST/Haydock.pm
@@ -1,5 +1,5 @@
 package Photonic::LE::ST::Haydock;
-$Photonic::LE::ST::Haydock::VERSION = '0.021';
+$Photonic::LE::ST::Haydock::VERSION = '0.022';
 
 =encoding UTF-8
 
@@ -9,7 +9,7 @@ Photonic::LE::S::Haydock
 
 =head1 VERSION
 
-version 0.021
+version 0.022
 
 =head1 COPYRIGHT NOTICE
 
